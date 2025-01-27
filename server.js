0720bed2abfe49fa984b447a39f96901bb287d87
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-01-27 17:43:33 +0530

    added new disconnect functionlity with css

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.on("connection",(socket) =>{
 //HANDLES WHEN A USER WILL JOIN
 socket.on('join',(username)=>{
     users.add(username); //add user to the users list
-
+    socket.username = username;
     //BROADCAST TO ALL CLINET/USERS ABOUT NEW USER JOINED
     io.emit('userJoined',username);
 
@@ -31,7 +31,23 @@ socket.on("chatMessage",(message)=>{
     console.log("mesage Recived",message); //debug
     
     io.emit("chatMessage",message); //this will emit back the message for all users
+});
+
+//HANDLE THE DISCONNECTION OF USER
+socket.on("disconnect",()=>{
+    console.log("An user is disconnect");
+    
+    users.forEach(user =>{
+        if(user === socket.username){
+            users.delete(user);
+
+            //emit the broad for the main html
+            io.emit('userleft',user);
+            io.emit('userslist',Array.from(users)) //emit the update user list
+        }
+    })
 })
+
 })
 //MIDDLEWARE TO READ THE /PUBLIC HTML DOCUMENT 
 app.use(express.static('public'));
