d4f3011a57fb71d6ba3d0f4572982538e3ea9cbb
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-01-27 08:23:38 +0530

    Done till message sending and reciving

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const http  = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+
+//create a http server
+
+const server = http.createServer(app);
+
+//INTIATE SOCKETIO AND ATTACH TO HTTP SERVER\
+const io = socketIo(server);
+
+const users = new Set();
+
+io.on("connection",(socket) =>{
+    console.log("A user is now connected");
+
+//HANDLES WHEN A USER WILL JOIN
+socket.on('join',(username)=>{
+    users.add(username); //add user to the users list
+
+    //BROADCAST TO ALL CLINET/USERS ABOUT NEW USER JOINED
+    io.emit('userJoined',username);
+
+    //send updated users list
+    io.emit('userslist',Array.from(users));
+})
+//HANDLES INCOMING CHAT MESSAGE
+socket.on("chatMessage",(message)=>{
+    console.log("mesage Recived",message); //debug
+    
+    io.emit("chatMessage",message); //this will emit back the message for all users
+})
+})
+//MIDDLEWARE TO READ THE /PUBLIC HTML DOCUMENT 
+app.use(express.static('public'));
+//LISITEN TO PORT
+const PORT =2929
+server.listen(PORT,()=>{
+    console.log('Server is listining on port 2929')
+});
